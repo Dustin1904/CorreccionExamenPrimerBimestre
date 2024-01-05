8fc3a38425bd6f9b33916035aed213ec5a1175cd
Dustin <dustin04ms@gmail.com>
2024-01-05 12:36:38 -0500

    Update Main.java

--- Correccion/src/Main.java
+++ Correccion/src/Main.java
@@ -5,24 +5,31 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Ingrese xd: ");
-        String info = sc.nextLine();
+        try{
+            System.out.println("Ingrese xd: ");
+            String info = sc.nextLine();
 
-        Videojuegos gen = new Videojuegos(info);
+            Videojuegos gen = new Videojuegos(info);
 
-        System.out.println("Ingrese una descpricion: ");
-        String dato1=sc.nextLine();
-        Genero gen1=new Genero(dato1);
+            System.out.println("Ingrese una descpricion: ");
+            String dato1=sc.nextLine();
+            Genero gen1=new Genero(dato1);
 
-        System.out.println("Ingrese una descripcion al genero Aventura: ");
-        String dato2= sc.nextLine();
-        System.out.println("Igrese un ejemplo del genero aventura: ");
-        String dato3= sc.nextLine();
-        tipos tip=new tipos(dato2,dato3);
+            System.out.println("Ingrese una descripcion al genero Aventura: ");
+            String dato2= sc.nextLine();
+            System.out.println("Igrese un ejemplo del genero aventura: ");
+            String dato3= sc.nextLine();
+            tipos tip=new tipos(dato2,dato3);
 
-        gen1.imprimir();
-        gen.imprimir();
-        tip.imprimir();
-        
+            gen1.imprimir();
+            gen.imprimir();
+            tip.imprimir();
+        }
+        catch (Exception e){
+            System.out.println("Los valores ingresados no pueden ser Numeros");
+        }
+
+
+
     }
 }
